a95a6a0cf1cfcd2bcee037fe3cd50ee817da8b0f
salonibulch <56321031+salonibulch@users.noreply.github.com>
2019-10-08 16:08:07 -0400

    display name and breed for each animal

--- php-animals/all-animals.php
+++ php-animals/all-animals.php
@@ -19,22 +19,22 @@
     if (($myrow = $st->fetch())) {
 ?>
 <form method="post" action="drinker-info.php">
-Select a drinker below to view more information:<br/>
+Select an animal below to view more information:<br/>
 <?php
       do {
         // echo produces output HTML:
-        echo "<input type='radio' name='drinker' value='" . $myrow['name'] . "'/>";
+        echo "<input type='radio' name='name'+' '+'breed' value='" . $myrow['name'] . "'/>";
         echo $myrow['name'] . "<br/>";
       } while ($myrow = $st->fetch());
       // Below we will see the use of a "short open tag" that is equivalent
       // to echoing the enclosed expression.
 ?>
-<?= $st->rowCount() ?> drinker(s) found in the database.<br/>
+<?= $st->rowCount() ?> animals(s) found in the database.<br/>
 <input type="submit" value="GO!"/>
 </form>
 <?php
     } else {
-      echo "There is no drinker in the database.";
+      echo "There is no animal in the database.";
     }
   } catch (PDOException $e) {
     print "Database error: " . $e->getMessage() . "<br/>";
